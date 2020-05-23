60691ffc3c6f760aa584a306f970ac7e48a461c9
xiaoxiaokay <xiaoxiaokay@foxmail.com>
2020-05-23 10:42:42 +0800

    加入创建数据库和表的各项配置

--- app/src/main/java/com/beiyanweather/android/MainActivity.java
+++ app/src/main/java/com/beiyanweather/android/MainActivity.java
@@ -4,6 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * @author: 潇潇
+ * @project: 贝言天气
+ * @Package com.beiyanweather.android
+ * @Description: 天气预报
+ * @date: 2020/5/23
+ * @version: V1.0
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
---
+++ app/src/main/java/com/beiyanweather/android/db/City.java
@@ -0,0 +1,41 @@
+package com.beiyanweather.android.db;
+
+public class City {
+
+    private int id;
+    private String cityName;    // 记录市的名字
+    private int cityCode;       // 记录市的代号
+    private int provinceId;     // 记录当前市所属省的id值
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
