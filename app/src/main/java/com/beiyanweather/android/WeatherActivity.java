5506d1960e7dc60a2d03e4537ad102ecd25d9986
xiaoxiaokay <xiaoxiaokay@foxmail.com>
2020-05-24 00:52:34 +0800

    增加后台自动更新天气的功能。

--- app/src/main/java/com/beiyanweather/android/WeatherActivity.java
+++ app/src/main/java/com/beiyanweather/android/WeatherActivity.java
@@ -5,6 +5,7 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 
 import com.beiyanweather.android.gson.Forecast;
 import com.beiyanweather.android.gson.Weather;
+import com.beiyanweather.android.service.AutoUpdateService;
 import com.beiyanweather.android.util.HttpUtil;
 import com.beiyanweather.android.util.Utility;
 import com.bumptech.glide.Glide;
@@ -240,5 +242,7 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
